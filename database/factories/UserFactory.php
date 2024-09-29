a06056cc61712df8f3eb299215254d50244b3686
unknown <ardiandp>
2024-09-29 23:17:25 +0700

    database ok

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role_id' => rand(1, 5),
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,16 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Divisi;
+use App\Models\Role;
+use App\Models\Barang;
+use App\models\Supplier;
+use App\Models\BarangMasuk;
+use App\models\BarangKeluar;
+use App\Models\StokBarang;
+use App\Models\RequestBarang;
+use App\Models\Pembelian;
+use App\Models\BarangDiterima;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +23,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         Role::factory(5)->create();
+         User::factory(10)->create();
+         Divisi::factory(10)->create();
+         Barang::factory(10)->create();
+         Supplier::factory(10)->create();
+         BarangMasuk::factory(10)->create();
+         BarangKeluar::factory(10)->create();
+         StokBarang::factory(10)->create();
+         RequestBarang::factory(10)->create();
+         Pembelian::factory(10)->create();
+         BarangDiterima::factory(10)->create();
 
-        User::factory()->create([
+       /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
     }
 }
